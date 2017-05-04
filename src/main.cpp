24610c9fcfb4a9384a3eea1b729dbb1bd2c7a1b4
bobdenar <hugues@frodon.net>
2017-05-04 21:39:13 +0200

    Init

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include "Arduino.h"
+#include <Wire.h>
+#include <SPI.h>
+#include "Adafruit_SHT31.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+Adafruit_BMP280 bmp; // I2C
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial)
+    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("SHT31 test");
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+
+  Serial.println(F("BMP280 test"));
+
+  if (!bmp.begin(0x76)) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+
+}
+
+
+void loop() {
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+
+  if (! isnan(t)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.println(t);
+  } else {
+    Serial.println("Failed to read temperature");
+  }
+
+  if (! isnan(h)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(h);
+  } else {
+    Serial.println("Failed to read humidity");
+  }
+  Serial.println();
+
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print(F("Pressure = "));
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.println();
+
+  delay(10000);
+}
