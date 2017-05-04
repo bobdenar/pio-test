befdcd40b82034aa7f778ea1989f8fac63e6893a
bobdenar <hugues@frodon.net>
2017-05-04 23:00:07 +0200

    Wifi upload

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,18 @@
 #include "Adafruit_SHT31.h"
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 Adafruit_BMP280 bmp; // I2C
+
+const char* ssid = "<ssid>";
+const char* password = "<password>";
 
 void setup() {
   Serial.begin(9600);
@@ -28,10 +36,57 @@ void setup() {
     while (1);
   }
 
+
+
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+
 }
 
 
 void loop() {
+
+  ArduinoOTA.handle();
+
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
 
@@ -57,6 +112,9 @@ void loop() {
   Serial.println(" Pa");
 
   Serial.println();
-
-  delay(10000);
+  for(int y=0; y<10; y++)
+  {
+    ArduinoOTA.handle();
+    delay(1000);
+  }
 }
